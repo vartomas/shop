85e35490ffa9e85b8f7272245ef7fbeabebbd7b3
Tomas <tomasvareika85@gmail.com>
2023-02-23 10:04:47 +0200

    close modal on auth change

--- src/components/UserModal.tsx
+++ src/components/UserModal.tsx
@@ -1,5 +1,5 @@
-import { FC } from 'react';
-import { useUser } from '@supabase/auth-helpers-react';
+import { FC, useEffect } from 'react';
+import { useSupabaseClient, useUser } from '@supabase/auth-helpers-react';
 import { MdClose } from 'react-icons/md';
 import IconButton from './IconButton';
 import Login from './Login';
@@ -12,12 +12,17 @@ interface Props {
 
 const UserModal: FC<Props> = ({ open, onClose }) => {
   const user = useUser();
+  const supabaseClient = useSupabaseClient();
+
+  useEffect(() => {
+    supabaseClient.auth.onAuthStateChange(() => onClose());
+  }, []);
 
   if (!open) return null;
 
   return (
-    <div className="user-modal-overlay" onClick={onClose}>
-      <div className="user-modal" onClick={(e) => e.stopPropagation()}>
+    <div className="user-modal-overlay">
+      <div className="user-modal">
         <div className="user-modal__close-btn">
           <IconButton Icon={MdClose} onClick={onClose} />
         </div>
