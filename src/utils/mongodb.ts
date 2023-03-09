f59667a7abcab53969de29f57cc8f4efe87ee109
Tomas <tomasvareika85@gmail.com>
2023-03-09 17:59:09 +0200

    add mongo url for prod

--- src/utils/mongodb.ts
+++ src/utils/mongodb.ts
@@ -1,4 +1,10 @@
 import mongoose from 'mongoose';
 
 export const connect = async () =>
-  await mongoose.connect(process.env.MONGODB_URI as string).catch((error) => console.log(error));
+  await mongoose
+    .connect(
+      process.env.NODE_ENV === 'development'
+        ? (process.env.MONGODB_URI as string)
+        : (process.env.MONGODB_URI_PROD as string)
+    )
+    .catch((error) => console.log(error));
