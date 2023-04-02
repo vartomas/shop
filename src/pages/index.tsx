9b5ad546892f8ab92592046a609d410538df7cd0
Tomas <tomasvareika85@gmail.com>
2023-04-02 17:43:29 +0300

    home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
+import Link from 'next/link';
+import Button from '@/components/Button';
+
 const Home = () => {
-  return <>home pipe tst</>;
+  return (
+    <div className="home">
+      <h1 className="home__header">Welcome to localghost shop!</h1>
+      <Link href="/products">
+        <Button title="Browse products" />
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
